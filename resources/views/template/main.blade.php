ad16ad139c7aa0609497b095846df22452f37f18
Gianclaudio <gianclaudiotarantini@gmail.com>
2023-05-18 22:11:57 +0200

    esercizio6

--- resources/views/template/main.blade.php
+++ resources/views/template/main.blade.php
@@ -7,7 +7,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
         <meta name="description" content />
         <meta name="author" content />
-        <title>Modern Business - Start Bootstrap Template</title>
+        <title>Laravel esercizio 6</title>
         @vite(['resources/css/app.css', 'resources/js/app.js'])
 
     </head>
